f63d990ce373f3e98c4cfae794bdda3ea8def2f6
Matt Hunter <10552383+mattshunter@users.noreply.github.com>
2026-08-25 17:56:25 -0400

    Added chatbot and streaming response examples.

---
+++ chatbot/java/chatbot/src/main/java/it/icanhazmatt/Chatbot.java
@@ -0,0 +1,93 @@
+package it.icanhazmatt;
+
+import com.anthropic.client.AnthropicClient;
+import com.anthropic.client.okhttp.AnthropicOkHttpClient;
+import com.anthropic.models.messages.MessageCreateParams;
+import com.anthropic.models.messages.MessageParam;
+import com.anthropic.models.messages.TextBlock;
+import com.anthropic.models.messages.MessageParam.Role;
+
+import static com.anthropic.models.messages.MessageParam.Role.ASSISTANT;
+import static com.anthropic.models.messages.MessageParam.Role.USER;
+import static com.anthropic.models.messages.Model.CLAUDE_SONNET_4_5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import lombok.extern.java.Log;
+
+/**
+ * Multiturn example!  This is just a simple example based 
+ * off the Building with the Claude API chat exercise (written
+ * in Python).
+ */
+@Log
+public class Chatbot {
+    private final AnthropicClient client;
+
+    private void addUserMessage(List<MessageParam> messageParams, String text) {
+        addMessage(USER, messageParams, text);
+    }
+
+    private void addAssistantMessage(List<MessageParam> messageParams, String text) {
+        addMessage(ASSISTANT, messageParams, text);
+    }
+
+    private void addMessage(Role role, List<MessageParam> messageParams, String text) {
+        messageParams.add(MessageParam.builder().role(role).content(text).build());
+    }
+
+    private Optional<TextBlock> chat(List<MessageParam> messageParams) {
+        var message = client.messages().create(
+            MessageCreateParams.builder()
+                .maxTokens(1000L)
+                .model(CLAUDE_SONNET_4_5)
+                .messages(messageParams)
+                .build()
+        );
+
+        return message.content().get(0).text();        
+    }
+
+    /**
+     * Default constructor to initialize the Anthropic client.  The environment values containing
+     * the Anthropic API key should be set external to the application runtime.
+      */
+    public Chatbot() {
+        this.client = AnthropicOkHttpClient.fromEnv();
+    }
+
+    public void startConversation() {
+        var messages = new ArrayList<MessageParam>();
+        var console = System.console();
+        var finished = false;
+        System.out.println("What can I help you with today?\n (type exit or stop to end our conversation.");
+        var text = "";
+        
+        do {
+            text = console.readLine("> ");
+            
+            if((text.equals("exit") || text.equals("stop"))) {
+                finished = true;
+            } else {
+                addUserMessage(messages, text);
+            
+                var answer = chat(messages);
+                answer.ifPresent(block -> {
+                    addAssistantMessage(messages, block.text());
+                    reply(block);
+                });
+            }
+        } while(!finished);
+    }
+
+    private void reply(TextBlock textBlock) {
+        log.info(textBlock.text());
+    }
+
+    public static void main(String...args) {
+        var app = new Chatbot();
+        app.startConversation();    
+    }    
+}
